ce82090a836ed039cfe4128ca48972d988fea4f2
Virág Kozsla <kozsla.virag@outlook.hu>
2021-08-31 19:16:01 +0200

    .

--- Szakdolgozat/Controllers/CateringController.cs
+++ Szakdolgozat/Controllers/CateringController.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols;
+using Newtonsoft.Json;
 using Szakdolgozat.Context;
 using Szakdolgozat.Models;
 
@@ -12,6 +15,8 @@ namespace Szakdolgozat.Controllers
 {
     public class CateringController : Controller
     {
+        List<int> catering = new List<int>();
+        List<object> cateringDataToChart = new List<object>();
         private readonly DataContext _context;
 
         public CateringController(DataContext context)
@@ -19,9 +24,101 @@ namespace Szakdolgozat.Controllers
             _context = context;
         }
 
+        public void ListUpload()
+        {
+            var cateringModel = _context.Catering.ToList();
+            foreach (var item in cateringModel)
+            {
+                catering.Add(item.SalesVolume);
+            }
+        }
+
+        public void ChartListUpload()
+        {
+            var cateringModel = _context.Catering.ToList();
+            foreach (var item in cateringModel)
+            {
+                cateringDataToChart.Add(item.Date);
+                cateringDataToChart.Add(item.SalesVolume);
+            }
+        }
+
+        public double Avg()
+        {
+
+            double avg = catering.Average();
+            return avg;
+        }
+
+        public int Median()
+        {
+            int salesCount = catering.Count();
+            int halfIndex = catering.Count() / 2;
+            var sortedNumbers = catering.OrderBy(n => n);
+            int median;
+            if ((salesCount % 2) == 0)
+            {
+                median = ((sortedNumbers.ElementAt(halfIndex) +
+                    sortedNumbers.ElementAt((halfIndex - 1))) / 2);
+                    
+            }
+            else
+            {
+                median = sortedNumbers.ElementAt(halfIndex);
+            }
+
+            return median;
+
+        }
+
+        public int Modus()
+        {
+            int mode = 0;
+
+            var groupResult = catering.GroupBy(n => n).ToList();
+
+            if (groupResult.Count != catering.Count)
+            {
+                mode = catering.GroupBy(n => n).
+                      OrderByDescending(g => g.Count()).
+                      Select(g => g.Key).FirstOrDefault();
+            }
+            else
+            {
+            }
+            return mode;
+        }
+
+        public int Max()
+        {
+            int max = catering.Max();
+            return max;
+        }
+
+        public int Min()
+        {
+            int min = catering.Min();
+            return min;
+        }
+
+        public double Deviation()
+        {
+            double avg = Avg();
+            List<double> differences = new List<double>();
+            foreach (var item in catering)
+            {
+                double difference = Math.Pow(item - avg,2);
+                differences.Add(difference);
+            }
+            double differenceSum = differences.Sum();
+            double deviation = Math.Sqrt(differenceSum/catering.Count());
+
+            return deviation;           
+        }
+
         // GET: Catering
         public async Task<IActionResult> Index()
-        {
+        {            
             return View(await _context.Catering.ToListAsync());
         }
 
@@ -44,6 +141,20 @@ namespace Szakdolgozat.Controllers
         }
 
         // GET: Catering/Create
+
+        public IActionResult Statistics()
+        {
+            ChartListUpload();
+            ViewBag.CateringModels = JsonConvert.SerializeObject(cateringDataToChart);
+            ListUpload();
+            ViewBag.avg = Avg();
+            ViewBag.median = Median();
+            ViewBag.modus = Modus();
+            ViewBag.max = Max();
+            ViewBag.min = Min();
+            ViewBag.dev = Deviation();
+            return View();
+        }
         public IActionResult Create()
         {
             return View();
--- Szakdolgozat/Models/CateringModel.cs
+++ Szakdolgozat/Models/CateringModel.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
+using Szakdolgozat.Context;
+using Szakdolgozat.Controllers;
 
 namespace Szakdolgozat.Models
 {
+    [DataContract]
     public class CateringModel
     {
+        public CateringModel(DateTime Date, int SalesVolume)
+        {
+            this.Date = Date;
+            this.SalesVolume = SalesVolume;
+
+        }
+        //private readonly DataContext _context;
+
         [Required]
         [Key]
         [Display(Name ="Dátum")]
@@ -18,5 +30,8 @@ namespace Szakdolgozat.Models
         [Display(Name ="Eladási forgalom (millió Ft)")]
         [DisplayFormat(DataFormatString = "{0:N0} Ft")]
         public int SalesVolume { get; set; }
+        
+                
+
     }
 }
